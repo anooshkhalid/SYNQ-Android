99fb659b7f62a2bfd4f309fa5fa44eee9c751717
Kjartan <kjartanvestvik@gmail.com>
2016-11-10 12:45:30 +0100

    SynqUploadHandler: modify interface methods

--- synquploader/src/main/java/fm/synq/synquploader/SynqUploadHandler.java
+++ synquploader/src/main/java/fm/synq/synquploader/SynqUploadHandler.java
@@ -6,6 +6,21 @@ package fm.synq.synquploader;
 
 public interface SynqUploadHandler {
 
-    void onCompleted(Exception e, String result);
+    /**
+     * Returns when the request finished successfully
+     */
+    void onCompleted();
+
+    /**
+     * Returns when the request failed due to a network error
+     * or because the server returned an HTTP error.
+     *
+     * @param error     Error message
+     */
+    void onFailure(String error);
+
+    /**
+     * Reports the file upload progress periodically
+     */
     void onProgress(long bytesTransferred, long totalSize);
 }
